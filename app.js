7612ecdfd5f37a2607db9e6aa66eb9f5d2b0815e
James Huynh <mrpaperman@live.ca>
2021-10-03 13:14:26 -0400

    Display initial frontend

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express();
+const port = process.env.PORT || 3000;
 
-console.log("I am running!")
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log("I am running!");
+});
