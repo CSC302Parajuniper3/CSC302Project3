c3beb783ae8a9880e4048089916a81c363e5d5c1
Henry G <henry.wenhan.gao@gmail.com>
2021-10-26 01:32:39 -0400

    Added janky code for list directories.

---
+++ api/tests/listDirectories.test.js
@@ -0,0 +1,61 @@
+const server = require('../../app.js');
+const supertest = require('supertest');
+const request = supertest(server.app_server);
+
+var activityModel;
+var planModel;
+
+jest.setTimeout(300000);
+
+beforeAll(() => {
+  activityModel = server.activityModel;
+  planModel = server.planModel;
+})
+
+beforeEach(() => {
+  activityModel.remove({}, function(err) {});
+  planModel.remove({}, function(err) {});
+})
+
+test("Bad query", (done) => {
+  request.get('/listDefinitions').query({ resourceType: 'wagwag' }) .expect(404, function(err, res) {
+    expect(res.body.error).toEqual("Resource type invalid.");
+    done();
+  });
+});
+
+test("One activity", (done) => {
+  var activity = new activityModel({ id: "ex1" });
+  activity.save(function (err, activity) {
+    if (err) return console.error(err);
+  });
+
+  var plan = new planModel({ id: "ex2" });
+  plan.save(function (err, plan) {
+    if (err) return console.error(err);
+  });
+
+  request.get('/listDefinitions').query({ resourceType: 'ActivityDefinition' }) .expect(200, function(err, res) {
+    expect(res.body.ids.sort()).toEqual(["ex1"].sort());
+  });
+
+  request.get('/listDefinitions').query({ resourceType: 'PlanDefinition' }) .expect(200, function(err, res) {
+    expect(res.body.ids.sort()).toEqual(["ex2"].sort());
+    done();
+  });
+});
+
+test("No activities", (done) => {
+  request.get('/listDefinitions').query({ resourceType: 'ActivityDefinition' }) .expect(200, function(err, res) {
+    expect(res.body.ids).toEqual([]);
+  });
+
+  request.get('/listDefinitions').query({ resourceType: 'PlanDefinition' }) .expect(200, function(err, res) {
+    expect(res.body.ids).toEqual([]);
+    done();
+  });
+});
+
+afterAll(() => {
+  server.close();
+})
--- app.js
+++ app.js
@@ -2,10 +2,63 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+
+var activityModel = null;
+var planModel = null;
+
+var events = require('events');
+
+function load_db() {
+    mongoose.connect('mongodb://127.0.0.1:27017/test');
+    
+    const activitySchema = new mongoose.Schema({ id: 'string' });
+    activityModel = mongoose.model('Activity', activitySchema);
+
+    const planSchema = new mongoose.Schema({ id: 'string' });
+    planModel = mongoose.model('Plan', planSchema);
+  }
+
+load_db();
+
+app.get('/listDefinitions', (req, res) => {
+  let resourceType = req.query.resourceType;
+  let model = null;
+
+  if (resourceType === "ActivityDefinition") {
+    model = activityModel;
+  } else if (resourceType === "PlanDefinition") {
+    model = planModel;
+  } else {
+    res.status(404).send({error: "Resource type invalid."});
+    return;
+  }
+
+  if (model == null) {
+    res.status(500).send({error: "An error occurred internally. Please try again later."});
+    return;
+  }
+
+  model.distinct('id', function(err, listIds) {
+    res.send({ids: listIds});
+  });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+var app_server = app.listen(port, () => {
   console.log("I am running!");
 });
+
+
+function close() {
+  mongoose.disconnect();
+  app_server.close();
+}
+
+module.exports.close = close;
+module.exports.activityModel = activityModel
+module.exports.planModel = planModel
+module.exports.app_server = app_server
