b74c988e9d3fd4e6c059d73f83727eaba41d6efb
dgao99 <danielgaorj@gmail.com>
2021-11-30 15:41:47 -0500

    db definition schemas

--- models/activity-definition.js
+++ models/activity-definition.js
@@ -1,13 +1,42 @@
 const mongoose = require('mongoose');
 
-// todo: fill in activity definition schema
 const ActivityDefinitionSchema = new mongoose.Schema({
     id: {
         type: String,
         minLength: 1,
         unique: true,
         required: true
-    }
+    },
+    resourceType: {
+        type: String,
+        minLength: 10,
+        default: "ActivityDefinition"
+    },
+    meta: {},
+    extension: [],
+    url: String,
+    version: { 
+        type: String, 
+        default: "1.0.0"
+    },
+    name: String,
+    title: String,
+    status: String,
+    experimental: Boolean,
+    date: Date,
+    updated: {
+        type: Date,
+        default: Date.now()
+    },
+    publisher: String,
+    description: String,
+    jurisdiction: [],
+    kind: String,
+    profile: String,
+    code: {},
+    intent: String,
+    doNotPerform: Boolean,
+    dynamicValue: []
 });
 
 const ActivityDefinition = mongoose.model('ActivityDefinition', ActivityDefinitionSchema);
--- models/plan-definition.js
+++ models/plan-definition.js
@@ -1,13 +1,39 @@
 const mongoose = require('mongoose');
 
-// todo: fill in plan definition schema
 const PlanDefinitionSchema = new mongoose.Schema({
     id: {
         type: String,
         minLength: 1,
         unique: true,
         required: true
-    }
+    },
+    resourceType: {
+        type: String,
+        minLength: 10,
+        default: "PlanDefinition"
+    },
+    meta: {},
+    extension: [],
+    url: String,
+    version: { 
+        type: String, 
+        default: "1.0.0"
+    },
+    name: String,
+    title: String,
+    type: {},
+    status: String,
+    experimental: Boolean,
+    date: Date,
+    updated: {
+        type: Date,
+        default: Date.now()
+    },
+    publisher: String,
+    description: String,
+    jurisdiction: [],
+    goal: [],
+    action: []
 });
 
 const PlanDefinition = mongoose.model('PlanDefinition', PlanDefinitionSchema);
